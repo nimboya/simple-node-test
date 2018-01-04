55d1c90bee810a69fb0f506cdf8455a5037fb240
Ewere Diagboya <nimboya@gmail.com>
2018-01-04 10:13:31 +0100

    update code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello Ewere, Welcome to your first node");
 
 });
 
